d04d957623c89fd346e11bfd0c1d364d0f226e7e
KenchOduah <79539489+KenchOduah@users.noreply.github.com>
2022-04-18 13:35:40 -0400

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+package Chapter6;
+//0. Do all the following in methods
+//1. Create an array and print the array
+//2. Print the first number in the array and then every other number(ex: print arr[0], arr[2], arr[4])
+//3. Print the sum of the arrays
+//4. Print the average of the arrays
+//5. Print the maximum and minimum values in an array
+//  ^ (this can be inside/outside a method(if inside a method do max and min individually)
+//
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		}
+	}
+
+}
